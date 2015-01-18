f4ff5f158788683505db1074cd81f3b52c718c7d
Schoewilliam <schoewilliam@gmail.com>
2015-01-18 09:57:23 +0100

    schcripts !

--- js/randomcolor.js
+++ js/randomcolor.js
@@ -7,3 +7,12 @@ var i,
 for (i = 0; i < total; i++) {
     allLinks[i].classList.add(colorpicked);
 }
+
+var schcripts = ['/images/logos/schcript1.png', '/images/logos/schcript2.png', '/images/logos/schcript3.png', '/images/logos/schcript4.png', '/images/logos/schcript5.png'];
+var pickedschcript = schcripts[Math.floor(Math.random() * schcripts.length)];
+
+var schcript = document.createElement("img");
+schcript.src = pickedschcript;
+schcript.alt = "Schoewilliam";
+document.getElementById("schtitle").appendChild(schcript);
+
