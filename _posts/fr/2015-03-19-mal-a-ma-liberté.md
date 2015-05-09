76f4e1fca309c89c0a83f2431f8db4cfaa605506
Schoewilliam <schoewilliam@gmail.com>
2015-05-09 02:38:48 +0200

    Dabeudidabeuda

--- _posts/fr/2015-03-19-mal-a-ma-liberté.md
+++
@@ -1,9 +0,0 @@
----
-layout: post
-published: true
-exacttime: "00:01:00"
-comments: false
-title: "« J'ai mal à ma liberté. »"
-category: "Société"
-link: "http://edhelas.movim.eu/blog/?post/2015/03/18/J-ai-mal-à-ma-liberté"
----
--- scripts/reading-time.js
+++
@@ -1,7 +0,0 @@
-// Reading time - jQuery script that rounds it to the nearest whole number
-
-$(document).ready(function() {
-    $(".reading-time").text(function (index, value) {
-      return Math.round(parseFloat(value));
-    });
-});
