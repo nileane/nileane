dad5488adf8c67b237565c2ff7eca991d1cad873
Schoewilliam <schoewilliam@gmail.com>
2015-05-18 14:29:46 +0200

    Flattr button

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -436,7 +436,7 @@
 
 
 smoothScroll.init({
-    speed: 500, // Integer. How fast to complete the scroll in milliseconds
+    speed: 400, // Integer. How fast to complete the scroll in milliseconds
     easing: 'easeInOutCubic', // Easing pattern to use
     updateURL: true, // Boolean. Whether or not to update the URL with the anchor hash on scroll
     offset: 0, // Integer. How far to offset the scrolling anchor location in pixels
